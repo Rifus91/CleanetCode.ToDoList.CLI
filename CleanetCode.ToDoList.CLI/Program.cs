6d73f16e8d4d172ac1e3c93c1dd510c8b8dd173e
Иван <Rifus91@mail.ru>
2022-03-07 23:32:49 +0300

    Добавьте файлы проекта.

---
+++ CleanetCode.ToDoList.CLI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CleanetCode.ToDoList.CLI
+{
+    class Program
+    {
+
+        class Task
+
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public bool IsCompleted { get; set; }
+            public DateTime DateOfCreation { get; set; }
+
+
+
+        }
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(1);
+
+        }
+    }
+}
